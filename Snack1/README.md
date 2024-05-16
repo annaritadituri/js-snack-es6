aff26decc3cc0f6d858172f6584732745439b956
annaritadituri <annaritadituri@icloud.com>
2024-05-16 23:13:26 +0200

    SNACK1

--- Snack1/README.md
+++ Snack1/README.md
@@ -5,3 +5,19 @@ Creare un array di oggetti:
 Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore utilizzando  template literal.
 -->
+
+-Creare un array di oggetti
+--Usare il metodo map per creare un array solo con i valori di peso
+---Creare una variabile per il valore della funzione minValue()
+----Stampare il valore relativo alla chiave "nome" dell'oggetto con minor peso
+
+<!--FUNZIONE minValue()-->
+Prende in input un array
+<!---->
+--Impostare min = il valore più grande assegnabile ad un int
+---Creare un ciclo for per gli elementi dell'array
+    SE il peso[i] < min
+        -min assumerà come nuovo valore il peso[i]
+----Creare un ciclo while per ritrovare la posizione corrispondente al min
+<!---->
+Return : un oggetto in cui salviamo la posizione e il valore min
--- Snack1/js/main.js
+++ Snack1/js/main.js
@@ -1,3 +1,68 @@
 'use strict';
 
 //////////////////////////////////////////////////////////
+
+//CREARE UN ARRAY DI OGGETTI
+const bikes = [
+    {
+        name: "Bianchi",
+        weight: 7.8
+    },
+
+    {
+        name: "Look",
+        weight: 8.0
+    },
+
+    {
+        name: "Pinarello",
+        weight: 7.5
+    }
+]
+
+console.log({bikes});
+
+//ARRAY DI VALORI DI PESO
+const weights = bikes.map((element) => {
+    return element.weight;
+})
+//console.log({weights});
+
+//VALORE MIN + POSIZIONE
+const minWeightPosition = minValue(weights);
+console.log({minWeightPosition});
+
+//STAMPARE IL NOME DEL MIN
+const position = (minWeightPosition.position - 1);
+console.log({position});
+console.log(`La bici con peso minore è la ${bikes[position].name} con ${bikes[position].weight}kg.`);
+
+////////////////////////////////////////////////////////
+
+//FUNZIONE minValue()
+function minValue(array) {
+
+    let min = 2147483647;
+    let pair;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            console.log(array[i]);
+            console.log({min});
+        }
+    }
+
+    let i = 0;
+    while (array[i] !== min) {
+        i++;
+    }
+
+    return pair = {
+        position: i + 1,
+        value: min
+    };
+
+}
+
+
